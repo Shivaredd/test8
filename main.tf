ca401936f9c384387dd14e35cf39120241ba74a7
shivaredd <buggaraghu@gmail.com>
2024-06-21 17:02:11 +0530

    main.tf

---
+++ main.tf
@@ -0,0 +1,30 @@
+resource "aws_instance" "web" {
+  ami           = var.ami
+  instance_type = var.instance_type
+  key_name      = var.key_name
+  vpc_security_group_ids = [aws_security_group.example.id]  # Corrected line
+
+  tags = {
+    Name = "Haiii"
+  }
+}
+
+resource "aws_security_group" "example" {
+  name        = "allow_tls"
+  description = "Allow TLS inbound traffic and all outbound traffic"
+  vpc_id      = var.vpc_id
+
+  egress {
+    from_port        = 0
+    to_port          = 0
+    protocol         = "-1"
+    cidr_blocks      = ["0.0.0.0/0"]
+  }
+
+  ingress {
+    from_port        = 0
+    to_port          = 0
+    protocol         = "-1"
+    cidr_blocks      = ["0.0.0.0/0"]
+  }
+}
