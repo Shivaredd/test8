6991274ecdb617324bf3fb9b55b97962705d321e
shivaredd <buggaraghu@gmail.com>
2024-06-21 17:05:45 +0530

    variables.tf

---
+++ variables.tf
@@ -0,0 +1,20 @@
+variable "ami" {
+    type = string
+    default = "ami-0c0e147c706360bd7"
+}
+
+variable "instance_type" {
+    type = string
+    default = "t3.micro"
+}
+
+variable "key_name" {
+    type = string
+    default = "raghu"
+}
+
+variable "vpc_id"{
+     type = string
+     default = "vpc-0c0229dff3dfb79f8""
+
+}
